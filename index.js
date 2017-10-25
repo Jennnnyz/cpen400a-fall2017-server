3220d95e353cca0ad616cf88646337d919933fcc
Kumseok Jung <jungkumseok@gmail.com>
2017-10-25 12:57:39 -0700

    - Updated properties on products to be consistent with assignment 3

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express')
 var app = express()
 
+var appHost = 'https://cpen400a-bookstore.herokuapp.com/'; //hard-coded host url (should really be defined in a separate config)
+
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
@@ -10,64 +12,76 @@ function getRandomInt(min, max) {
 
 var products = {
   'KeyboardCombo' : {
+	name: 'KeyboardCombo',
     price : getRandomInt(25,35),
     quantity : getRandomInt(0,10),
-    url : 'https://cpen400a-bookstore.herokuapp.com/images/KeyboardCombo.png'
+    imageUrl: appHost+'images/KeyboardCombo.png'
   },
   'Mice' : {
+	name: 'Mice',
     price : getRandomInt(5,7),
     quantity : getRandomInt(0,10),
-    url : 'https://cpen400a-bookstore.herokuapp.com/images/Mice.png'
+    imageUrl: appHost+'images/Mice.png'
   },
   'PC1' : {
+	name: 'PC1',
     price : getRandomInt(300,350),
     quantity : getRandomInt(0,10),
-    url : 'https://cpen400a-bookstore.herokuapp.com/images/PC1.png'
+    imageUrl: appHost+'images/PC1.png'
   },
   'PC2' : {
+	name: 'PC2',
     price : getRandomInt(350,400),
     quantity : getRandomInt(0,10),
-    url : 'https://cpen400a-bookstore.herokuapp.com/images/PC2.png'
+    imageUrl: appHost+'images/PC2.png'
   },
   'PC3' : {
+	name: 'PC3',
     price : getRandomInt(330,380),
     quantity : getRandomInt(0,10),
-    url : 'https://cpen400a-bookstore.herokuapp.com/images/PC3.png'
+    imageUrl: appHost+'images/PC3.png'
   },
   'Tent' : {
+	name: 'Tent',
     price : getRandomInt(30,40),
     quantity : getRandomInt(0,10),
-    url : 'https://cpen400a-bookstore.herokuapp.com/images/Tent.png'
+    imageUrl: appHost+'images/Tent.png'
   },
   'Box1' : {
+	name: 'Box1',
     price : getRandomInt(5,7),
     quantity : getRandomInt(0,10),
-    url : 'https://cpen400a-bookstore.herokuapp.com/images/Box1.png'
+    imageUrl: appHost+'images/Box1.png'
   },
   'Box2' : {
+	name: 'Box2',
     price : getRandomInt(5,7),
     quantity : getRandomInt(0,10),
-    url : 'https://cpen400a-bookstore.herokuapp.com/images/Box2.png'
+    imageUrl: appHost+'images/Box2.png'
   },
   'Clothes1' : {
+	name: 'Clothes1',
     price : getRandomInt(20,30),
     quantity : getRandomInt(0,10),
-    url : 'https://cpen400a-bookstore.herokuapp.com/images/Clothes1.png'
+    imageUrl: appHost+'images/Clothes1.png'
   },
   'Clothes2' : {
+	name: 'Clothes2',
     price : getRandomInt(20,30),
     quantity : getRandomInt(0,10),
-    url : 'https://cpen400a-bookstore.herokuapp.com/images/Clothes2.png'
+    imageUrl: appHost+'images/Clothes2.png'
   },
   'Jeans' : {
+	name: 'Jeans',
     price : getRandomInt(30,40),
     quantity : getRandomInt(0,10),
-    url : 'https://cpen400a-bookstore.herokuapp.com/images/Jeans.png'
+    imageUrl: appHost+'images/Jeans.png'
   },
   'Keyboard' : {
+	name: 'Keyboard',
     price : getRandomInt(15,25),
     quantity : getRandomInt(0,10),
-    url : 'https://cpen400a-bookstore.herokuapp.com/images/Keyboard.png'
+    imageUrl: appHost+'images/Keyboard.png'
   }
 };
 
